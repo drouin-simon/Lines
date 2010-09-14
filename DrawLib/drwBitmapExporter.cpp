5b5796bcea07d87baaf020c91372736bc2813ebe
Simon Drouin <drouin.simon@gmail.com>
2010-09-14 20:23:33 +0000

    Added commented example on how to use pixel buffers to render offscreen: it is not working.
    git-svn-id: file:///home/simon/NFBMigrationGit/CodeSvn/Code/trunk@10 d53ed5f0-1798-4b2c-8c60-f91484f69ffc

--- DrawLib/drwBitmapExporter.cpp
+++ DrawLib/drwBitmapExporter.cpp
@@ -1,4 +1,6 @@
 #include "drwBitmapExporter.h"
+#include <QGLFormat>
+#include <QGLPixelBuffer>
 
 drwBitmapExporter::drwBitmapExporter()
 {
@@ -11,6 +13,42 @@ drwBitmapExporter::~drwBitmapExporter()
 
 void drwBitmapExporter::run()
 {
+	//if (!QGLFormat::hasOpenGL() || !QGLPixelBuffer::hasOpenGLPbuffers())
+	//{
+	//	QMessageBox::information(0, "OpenGL pbuffers 2", "This system does not support OpenGL/pbuffers.");
+	//	return -1;
+	//}
+
+	/*QGLPixelBuffer * pbuffer = new QGLPixelBuffer(1024, 1024);
+	pbuffer->makeCurrent();
+
+	glMatrixMode( GL_PROJECTION );
+	glLoadIdentity();
+	gluOrtho2D( 0, 1024, 0, 1024 );
+	glMatrixMode( GL_MODELVIEW );
+	glLoadIdentity();
+
+	glClearColor( 0.0, 0.0, 1.0, 1.0 );
+	glClear( GL_COLOR_BUFFER_BIT );
+
+	glColor4d( 1.0, 0.0, 0.0, 1.0 );
+	glBegin(GL_QUADS);
+	{
+		glVertex2d( 100, 100 );
+		glVertex2d( 600, 100 );
+		glVertex2d( 600, 600 );
+		glVertex2d( 100, 600 );
+	}
+
+	glFlush();
+
+	pbuffer->doneCurrent();
+
+	QImage im = pbuffer->toImage();
+	im.save( "/home/simon/test.png" );
+
+	delete pbuffer;*/
+
 	for( int i = 0; i < 20; ++i )
 	{
 		emit WritingFrame( i );
