d9b295ae059fcf215628535746a3ffcfbccaf3ff
Simon Drouin <drouin.simon@gmail.com>
2011-04-28 04:21:22 +0000

    Vertex shader now supporting min line width.
    git-svn-id: file:///home/simon/NFBMigrationGit/CodeSvn/Code/trunk@67 d53ed5f0-1798-4b2c-8c60-f91484f69ffc

--- Examples/GlutTemplateDrawLib/main.cpp
+++ Examples/GlutTemplateDrawLib/main.cpp
@@ -1,6 +1,8 @@
 #include <IncludeGlut.h>
 #include <stdlib.h>
 #include "drwGlslShader.h"
+#include <sstream>
+#include "SVL.h"
 
 // original app params
 int winWidth = 800;
@@ -11,22 +13,47 @@ const char appName[] = "Glut Template";
 drwGlslShader shaderProgram;
 float lineRadius = 1.0;
 
+Vec2 point1( 50.0, 50.0 );
+Vec2 point2( 300.0, 300.0 );
+
+void RenderString( float x, float y, const char * string )
+{
+    glColor3f( 1.0, 1.0, 1.0 );
+    glRasterPos2f( x, y );
+    const char * it = string;
+    while( *it != 0 )
+    {
+        glutBitmapCharacter( GLUT_BITMAP_9_BY_15, *it );
+        ++it;
+    }
+}
+
 void display(void)
 {
 	glClear( GL_COLOR_BUFFER_BIT );
 	
 	shaderProgram.UseProgram( true );
 	shaderProgram.SetVariable( "line_radius", lineRadius );
+    shaderProgram.SetVariable( "min_line_radius", float(.5) );
+
+    Vec2 diff = point2 - point1;
+    diff.Normalise();
+    Vec2 left( -diff[1], diff[0] );
+    Vec2 right = -1.0 * left;
 	
 	glColor4d( 1.0, 0.0, 0.0, 0.0 );
 	glBegin( GL_QUADS );
-	glNormal3f( -.7071, .7071, 0 ); glVertex2d( 50, 50 );
-	glNormal3f( .7071, -.7071, 0 ); glVertex2d( 50, 50 );
-	glNormal3f( .7071, -.7071, 0 ); glVertex2d( 300, 300 );
-	glNormal3f( -.7071, .7071, 0 ); glVertex2d( 300, 300 );
+    glNormal3f( left[0], left[1], 0 ); glVertex2d( point1[0], point1[1] );
+    glNormal3f( right[0], right[1], 0 ); glVertex2d( point1[0], point1[1] );
+    glNormal3f( right[0], right[1], 0 ); glVertex2d( point2[0], point2[1] );
+    glNormal3f( left[0], left[1], 0 ); glVertex2d( point2[0], point2[1] );
 	glEnd();
 	
 	shaderProgram.UseProgram( false );
+
+    std::stringstream s;
+    s << "line radius: " << lineRadius;
+    RenderString( 300, 20, s.str().c_str() );
 
     glutSwapBuffers();
 }
@@ -66,14 +93,22 @@ void main() \
 
 static const char * vertexShaderCode = " \
 uniform float line_radius; \
+uniform float min_line_radius; \
 void main() \
 { \
 	vec4 scaled_normal = vec4( 0.0, 0.0, 0.0, 0.0 ); \
 	scaled_normal.xyz = gl_Normal; \
-	scaled_normal *= line_radius; \
+    float radius = line_radius; \
+    float damp_factor = 1.0; \
+    if( radius < min_line_radius ) \
+    { \
+        radius = min_line_radius; \
+        damp_factor = line_radius / min_line_radius; \
+    } \
+    scaled_normal *= radius; \
 	vec4 newVertex = gl_Vertex + scaled_normal; \
 	gl_Position = gl_ModelViewProjectionMatrix * newVertex; \
-	gl_FrontColor = gl_Color; \
+    gl_FrontColor = gl_Color * damp_factor; \
 }";
 
 
@@ -85,6 +120,7 @@ void init(void)
 		exit( -1 );
 	shaderProgram.UseProgram( true );
 	shaderProgram.SetVariable( "line_radius", lineRadius );
+    shaderProgram.SetVariable( "min_line_radius", float(.5) );
 	shaderProgram.UseProgram( false );
 	
 	glClearColor ( 0.0f, 0.0f, 0.0f, 0.0f);
@@ -110,6 +146,7 @@ void mouse(int button, int state, int x, int y)
    switch (button) 
    {
    case GLUT_LEFT_BUTTON:
+       point2 = Vec2( x, winHeight - y - 1 );
      break;
    }
    glutPostRedisplay();
@@ -133,17 +170,26 @@ void special(int key, int x, int y)
     switch (key) 
     {
     case GLUT_KEY_UP:
-			lineRadius += 1;
+        {
+            if( lineRadius >= 1.0 )
+                lineRadius += 1.0;
+            else
+                lineRadius *= 1.5;
 			glutPostRedisplay();
+        }
         break;
     case GLUT_KEY_LEFT:
 		break;
     case GLUT_KEY_RIGHT:
         break;
     case GLUT_KEY_DOWN:
-			if( lineRadius > 1 )
-				lineRadius -= 1;
+        {
+            if( lineRadius >= 2.0 )
+                lineRadius -= 1.0;
+            else
+                lineRadius *= .75;
 			glutPostRedisplay();
+        }
         break;
     }
 }
